4b31ac0230890f1f0774e780238e3706e74d1096
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 11:52:59 +0200

    task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated floor function"""
+import math
+
+
+def floor(n: float) -> float:
+    """Takes 1 float and return it's floor as float"""
+    return math.floor(n)
