20e836c0ddff3af5fb55ce083cdbac751b7fd8c5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 13:02:53 +0200

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated to_kv function"""
+from typing import Union, Tuple
+import math
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a str and an int or float and return a tuple"""
+    sq: Union[int, float] = math.pow(v, 2)
+    return (k, sq)
