29c00ed14010bd87baf928fc2dd1f61cb257c262
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 14:04:49 +0200

    task 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated safe_first_element function"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Takes a an element af Any type a return a it's first element"""
+    if lst:
+        return lst[0]
+    else:
+        return None
