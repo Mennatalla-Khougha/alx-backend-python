0acfb7655a9e4b9bbe3fbe6771c4ea0fe1161b49
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 12:48:56 +0200

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated sum_list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a float list and return their float sum"""
+    sum: float = 0.0
+    for i in input_list:
+        sum += i
+    return sum
