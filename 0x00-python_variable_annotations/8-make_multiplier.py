891b434b73ff5d7c12adcd39fa9857044f4bff31
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 13:16:37 +0200

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated make_multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Takes a float and return a float"""
+    def multiply(n: float) -> float:
+        """Takes a float and return a float"""
+        return n * multiplier
+    return multiply
