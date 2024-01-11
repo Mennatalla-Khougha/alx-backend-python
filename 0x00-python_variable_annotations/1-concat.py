a5ae1faf0c91b294f229730b67a3a185b2869f09
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 11:49:08 +0200

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module for a type annotated concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes 2 strings and return their concatenated string"""
+    return str1 + str2
