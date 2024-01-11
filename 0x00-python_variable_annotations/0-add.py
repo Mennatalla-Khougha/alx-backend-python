bd83bd970abab4062cdf4cf80f75d544f650db2b
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-11 11:37:22 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module for a type annotated add function"""
+
+
+def add(a: float, b: float) -> float:
+    """Takes 2 floats and return their sum as float"""
+    return a + b
