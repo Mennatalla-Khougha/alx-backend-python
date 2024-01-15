52244880e9c411913b47e923db6083d36773b2cd
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-15 14:05:11 +0200

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     The `measure_time` function measures the average time it takes to\
         execute the `wait_n` function `n` times with a maximum delay of\
