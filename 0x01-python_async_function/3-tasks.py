42b838e54ff7f93d9dbee9101a34abd6cd0ff950
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-15 14:12:56 +0200

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module define the wait_n function"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    The function `task_wait_random` creates an asyncio task that waits\
+        for a random amount of time.
+
+    :param max_delay: The `max_delay` parameter is an integer that represents\
+        the maximum delay in
+    seconds for the `wait_random` function
+    :type max_delay: int
+    :return: The function `task_wait_random` is returning an asyncio task\
+        object.
+    """
+    return asyncio.create_task(wait_random(max_delay))
