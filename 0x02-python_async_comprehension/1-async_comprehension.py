b617a747eab1139c2b3c64b0392b30f1b47ab31a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-16 14:40:20 +0200

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """This module define the async_comprehension function"""
-import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
@@ -11,6 +10,5 @@ async def async_comprehension() -> List[float]:
     values and returns a list of those values.
     :return: a list of values obtained from an async generator.
     """
-    gen = async_generator()
-    result = [value async for value in gen]
+    result = [value async for value in async_generator()]
     return result
