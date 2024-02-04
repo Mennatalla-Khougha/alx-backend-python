26c57a29a2cf07fd12d32833099286d75fb2e279
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-04 19:17:07 +0200

    task 8

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -102,3 +102,11 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient('org_name')
         repos = client.public_repos()
         self.assertEqual(repos, self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Integration test for clients with license filter"""
+        client = GithubOrgClient('org_name')
+        repos = client.public_repos(license="apache-2.0")
+        self.assertEqual(repos, self.apache2_repos)
+        
+        
